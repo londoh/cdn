4bfb7a2c207961e1df97e626003a6fcc675cb8bd
londoh <colin@flack.net>
2016-09-12 00:21:48 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,7 @@
 namespace Vinelab\Cdn\tests;
 
 // requiring this file to reference assertions as global functions. (to skip the $this->)
-// Im not sure about this
+// Im not sure about this but it will check if the fetch works
 require_once 'vendor/phpunit/phpunit/src/Framework/Assert/Functions.php';
 
 use PHPUnit_Framework_TestCase as PHPUnit;
